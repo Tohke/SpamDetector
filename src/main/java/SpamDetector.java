09ce62a3e7a84a21dad3b70cf86ebf03410a0527
Tohke <120936747+Tohke@users.noreply.github.com>
2025-05-11 16:35:20 -0300

    Add files via upload

---
+++ src/main/java/SpamDetector.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author JoãoFranciscoPatelli
+ */
+
+public class SpamDetector {
+    public static void main(String[] args) {
+        LerArquivo leitor = new LerArquivo();
+
+        String caminho = "C:\\Users\\Public\\Desktop\\Email.txt"; 
+        String conteudo = leitor.lerArquivo(caminho);
+
+        String[][] matriz = {
+            {"oferta", "0.9"},
+            {"grátis", "0.85"},
+            {"promoção", "0.8"},
+            {"compre", "0.75"},
+            {"dinheiro", "0.7"},
+            {"urgente", "0.6"}
+        };
+
+        double peso = 0.0;
+        String conteudoLower = conteudo.toLowerCase();
+
+        for (int i = 0; i < matriz.length; i++) {
+            String palavra = matriz[i][0];
+            String pesoString = matriz[i][1];
+
+            double pesoDouble = Double.parseDouble(pesoString);
+            int pesoInt = (int)(pesoDouble * 10);
+
+            if (conteudoLower.contains(palavra)) {
+                peso += pesoInt;
+            }
+        }
+
+        System.out.println("Pontuação final (inteira): " + peso);
+
+
+        if (peso > 70) {
+            System.out.println("Este e-mail é fortemente considerado SPAM.");
+        } 
+        else if (peso >= 31 && peso <= 70) {
+            System.out.println("Este e-mail é potêncialmente considerado SPAM.");
+        }
+        else if (peso >= 5 && peso <31){
+            System.out.println("Este e-mail é fracamente considerado SPAM.");
+        }
+        else{
+            System.out.println("Este e-mail não é considerado SPAM");
+        }
+    }
+}
